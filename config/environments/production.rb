788ac8b52040c1f3731b6a3da2698ad272effb8b
Bart Breeschoten <foobart@gmail.com>
2014-05-22 11:19:35 +0200

    added imagemagick fix for windows server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,14 @@ Ddg::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # needed to get Imagemagick working on the windows server
+  initializer 'override-image-magick-paths', :after => 'attach-refinery-images-with-dragonfly' do
+    app=Dragonfly[:refinery_images]
+    app.configure_with(:imagemagick)
+    app.configure do |c|
+      c.convert_command = "C:/Progra~1/ImageMagick-6.8.9-Q16/convert" # defaults to "convert"
+      c.identify_command = "C:/Progra~1/ImageMagick-6.8.9-Q16/identify"  # defaults to "identify"
+    end
+  end  
 end
