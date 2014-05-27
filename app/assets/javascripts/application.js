24111294f3beab3d33741e7f5f81aae154bc9ca1
Bart Breeschoten <foobart@gmail.com>
2014-05-27 10:02:27 +0200

    nogmaals zonder asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+$(document).foundation('orbit');
 $(document).foundation();
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Ddg::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -47,7 +47,7 @@ Ddg::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( vendor/modernizr.js )
+  # config.assets.precompile += %w( vendor/modernizr.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
