866499cd3adef86b77c3d4ee5f6cce840539d901
Bart Breeschoten <foobart@gmail.com>
2014-05-27 10:10:02 +0200

    Revert 6ca0ee1..4093405
    This rolls back to commit 6ca0ee1dd7aa059a838e35e84731f36425e56bd7.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Ddg
@@ -60,7 +60,7 @@ module Ddg
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
