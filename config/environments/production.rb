ce9ca82e8475fa8f4892e4f6994fa7551b59e381
Bart Breeschoten <foobart@gmail.com>
2014-05-27 13:32:21 +0200

    added refinery css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Ddg::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( vendor/modernizr.js )
+  config.assets.precompile += ['refinery/refinery.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
